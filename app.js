2b9ff8e5e91bdff7aa87305434b803ca8b416f22
Oscar <ew12136@gmail.com>
2024-12-03 01:17:57 +0800

    C4-M1-Create：implement creating restaurant list

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.engine('.hbs', engine({extname: '.hbs'}))
 // app.set：透過這個方法告訴 Express 說要設定的 view engine 是 hbs (handlebars)。
 app.set('view engine', '.hbs') 
 app.set('views', './views')
+app.use(express.urlencoded({ extended: true })) // Create：app.use 的使用方式在後面的章節會搭配 middleware 有詳細說明，這邊可以先簡單理解 app.use 是用來對「所有的 request」進行前置處理即可
+// 由於 Express.js 如果要獲取傳送過來的表單資料需要另外設定，否則會回傳 undefined，因此，這裡需要使用 express.urlencoded，從請求網址中獲取表單資料，並加入 extended: true 的設定
 
 // 新增 for MVC 的 Model
 // 在 app.js 檔案使用 app.use，並提供靜態檔案的路徑，就可以將 JSON 檔案載入
@@ -50,13 +52,40 @@ app.get('/restaurants', (req, res) => {
 
 // create-取得 GET restaurants 新增頁：http://localhost:3000/restaurants/new
 app.get('/restaurants/new', (req, res) => {
-	res.send('create restaurant')
+	return res.render('new') // 刪除原本的「res.send('create restaurant')」，改成「return res.render('new')」，以「透過 view 引擎去拿 new 樣板」
 })
 
 // create-新增 POST restaurant：http://localhost:3000/restaurants/new
-app.post('/restaurants', (req, res) => { // 建立、新增 restaurant
-	res.send('add restaurant')
-})
+app.post('/restaurants', (req, res) => {
+  // 刪除原本的「res.send('add restaurant')」
+  // 從表單中獲取資料
+  const { // 原本是 const name = req.body.RestaurantName // 表單資料可以從 request 的 body 得到，並依據剛剛我們在 Input 中指定的 Name 屬性來定義
+    RestaurantName,
+    RestaurantName_en,
+    RestaurantCategory,
+    RestaurantImage,
+    RestaurantLocation,
+    RestaurantPhone,
+    RestaurantGoogle_map,
+    RestaurantRating,
+    RestaurantDescription,
+  } = req.body;
+
+  // 使用 Sequelize 的 Model Create 方法插入資料
+  return restaurant.create({ // 原本是 return restaurant.create({ name })
+    name: RestaurantName,
+    name_en: RestaurantName_en,
+    category: RestaurantCategory,
+    image: RestaurantImage,
+    location: RestaurantLocation,
+    phone: RestaurantPhone,
+    google_map: RestaurantGoogle_map,
+    rating: RestaurantRating,
+    description: RestaurantDescription,
+  })
+    .then(() => res.redirect('/restaurants')) // 成功後用 redirect，將網址導回 restaurants 清單頁
+    .catch((err) => console.error(err));
+});
 
 // Read-取得 GET restaurants 項目頁-看 restaurant 的 detail：http://localhost:3000/restaurants/{id}
 app.get('/restaurants/:id', (req, res) => {
